820eb9b4c66db6b3e9ca6fd37e488d0e64278dd7
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-10 13:47:40 -0400

    first half-ish done

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,55 @@
 # your code goes here
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(num)
+    @happiness = calculate_score(num)
+  end
+
+  def hygiene=(num)
+    @hygiene = calculate_score(num)
+  end
+
+  def clean?
+    @hygiene > 7
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def get_paid(amount)
+    @bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def calculate_score(num)
+    if num < 0 
+      0
+    elsif num > 10
+      10
+    else 
+      num
+    end
+  end
+
+end
